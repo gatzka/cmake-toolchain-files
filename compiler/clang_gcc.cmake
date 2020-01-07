e34fca093cdb46eec525d2670c9c14d1f04683c8
Stephan Gatzka <stephan.gatzka@hbm.com>
2020-01-07 12:55:19 +0100

    Define flags for profiling.

--- compiler/clang_gcc.cmake
+++ compiler/clang_gcc.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -pipe -fno-common -Wall -Wextra -Wshadow -Winit-self -Wcast-qual -Wcast-align -Wformat=2 -Wwrite-strings -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition -Wstrict-overflow=5 -Wdisabled-optimization -Wmissing-include-dirs -Wswitch-default -Wundef -Wconversion -pedantic -pedantic-errors")
 
 set(COVERAGE_FLAGS "--coverage")
-
+set(PROFILE_FLAGS "-pg -fno-omit-frame-pointer")
 set(ASAN_FLAGS "-fsanitize=address -fno-sanitize-recover=all -fno-omit-frame-pointer")
 
 # Don't use leak sanitizer and undefined behavior sanitizer in ctest CI for gcc. gcc does not write a logfile on which ctest relies on.
