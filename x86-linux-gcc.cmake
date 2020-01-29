878c80a6745bb32e5bd4a5e6d8e8a03103005aff
Stephan Gatzka <stephan.gatzka@gmail.com>
2020-01-29 08:26:04 +0100

    Use gcc 9 settings for default gcc toolchain file.

--- x86-linux-gcc.cmake
+++ x86-linux-gcc.cmake
@@ -1,6 +1,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
-include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc8.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc9.cmake")
 
 set(CMAKE_C_COMPILER gcc)
 set(CMAKE_CXX_COMPILER g++)
