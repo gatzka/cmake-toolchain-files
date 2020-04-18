d0f06566878e564fa8771704b764daf6a12f1042
Stephan Gatzka <stephan.gatzka@gmail.com>
2020-04-18 14:45:56 +0200

    Add newer cross toolchain files.

---
+++ arm-linux-gnueabihf-gcc-9.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc8.cmake")
+
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc-9)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++-9)
+set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-arm" "-L" "/usr/arm-linux-gnueabihf/")
---
+++ powerpc64-linux-gcc-9.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR ppc)
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc8.cmake")
+
+set(CMAKE_C_COMPILER powerpc64-linux-gnu-gcc-9)
+set(CMAKE_CXX_COMPILER powerpc64-linux-gnu-g++-9)
+set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-ppc64" "-L" "/usr/powerpc64-linux-gnu/")
