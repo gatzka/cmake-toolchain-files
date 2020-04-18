edb4c9d3a5b6f1de38cc082e83b5e0fb7434599f
Stephan Gatzka <stephan.gatzka@gmail.com>
2020-04-18 15:43:16 +0200

    Enable new warnings for clang-10.

--- x86-linux-clang-10.cmake
+++ x86-linux-clang-10.cmake
@@ -1,6 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
 include("${CMAKE_CURRENT_LIST_DIR}/compiler/clang.cmake")
+set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -Wsizeof-array-div -Wtautological-overlap-compare -Wtautological-compare -Wtautological-bitwise-compare -Wbitwise-conditional-parentheses -Wmisleading-indentation -Wxor-used-as-pow")
 
 set(CMAKE_C_COMPILER clang-10)
 set(CMAKE_CXX_COMPILER clang++-10)
