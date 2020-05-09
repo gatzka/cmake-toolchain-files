66bcf07ab9caf6ac5ff1943ca92651178bf0fa98
Stephan Gatzka <stephan.gatzka@gmail.com>
2020-05-09 13:10:35 +0200

    Do not enable warnings twice.

--- compiler/clang_gcc.cmake
+++ compiler/clang_gcc.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -pipe -fno-common -Wall -Wextra -Wshadow -Winit-self -Wcast-qual -Wcast-align -Wformat=2 -Wwrite-strings -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition -Wstrict-overflow=5 -Wdisabled-optimization -Wmissing-include-dirs -Wswitch-default -Wundef -Wconversion -pedantic -pedantic-errors")
+set(CMAKE_C_FLAGS_INIT "-pipe -fno-common -Wall -Wextra -Wshadow -Winit-self -Wcast-qual -Wcast-align -Wformat=2 -Wwrite-strings -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition -Wstrict-overflow=5 -Wdisabled-optimization -Wmissing-include-dirs -Wswitch-default -Wundef -Wconversion -pedantic -pedantic-errors")
 
 set(COVERAGE_FLAGS "--coverage")
 set(PROFILE_FLAGS "-pg -fno-omit-frame-pointer")
