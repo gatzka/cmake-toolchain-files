a4ba43e77de3ccec1f3670fa5e2148100667e1be
Stephan Gatzka <stephan.gatzka@nexplore.com>
2023-04-06 10:29:24 +0200

    Add aarch64 toolchain file.

--- aarch64-linux-gnu-gcc-11.cmake
+++ aarch64-linux-gnu-gcc-11.cmake
@@ -1,11 +1,11 @@
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc11.cmake")
 
 set(CMAKE_C_FLAGS_INIT "${COMMON_WARN_FLAGS} ${C_WARN_FLAGS} -Wno-analyzer-null-dereference -Wno-analyzer-use-of-uninitialized-value -Wno-analyzer-possible-null-argument -Wno-analyzer-malloc-leak -Wno-analyzer-possible-null-dereference")
 set(CMAKE_CXX_FLAGS_INIT "${COMMON_WARN_FLAGS} ${CXX_WARN_FLAGS}")
 
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc-11 CACHE INTERNAL "C compiler")
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++-11 CACHE INTERNAL "C++ compiler")
-set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-arm" "-L" "/usr/arm-linux-gnueabihf/" CACHE INTERNAL "qemu")
+set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc-11 CACHE INTERNAL "C compiler")
+set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++-11 CACHE INTERNAL "C++ compiler")
+set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-aarch64" "-L" "/usr/aarch64-linux-gnu/" CACHE INTERNAL "qemu")
