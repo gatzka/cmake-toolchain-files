85149eb9cf6f3748caa3bf36a1e9162586871735
Stephan Gatzka <stephan.gatzka@gmail.com>
2024-07-30 12:29:34 +0200

    Bump arm ppc32 toolchain to latest versions.

---
+++ powerpc-linux-gcc-12.cmake
@@ -0,0 +1,11 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR ppc)
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc12.cmake")
+
+set(CMAKE_C_FLAGS_INIT "${COMMON_WARN_FLAGS} ${C_WARN_FLAGS}")
+set(CMAKE_CXX_FLAGS_INIT "${COMMON_WARN_FLAGS} ${CXX_WARN_FLAGS}")
+
+set(CMAKE_C_COMPILER powerpc-linux-gnu-gcc-12 CACHE INTERNAL "C compiler")
+set(CMAKE_CXX_COMPILER powerpc-linux-gnu-g++-12 CACHE INTERNAL "C++ compiler")
+set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-ppc" "-L" "/usr/powerpc-linux-gnu/" CACHE INTERNAL "qemu")
