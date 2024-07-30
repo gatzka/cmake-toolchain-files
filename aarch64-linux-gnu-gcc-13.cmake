fc0ffe2f4fc4f3d33a5878c73dbfbb964b19268d
Stephan Gatzka <stephan.gatzka@gmail.com>
2024-07-30 12:23:22 +0200

    Bump toolchain file to latest gcc versions.

---
+++ aarch64-linux-gnu-gcc-13.cmake
@@ -0,0 +1,11 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc13.cmake")
+
+set(CMAKE_C_FLAGS_INIT "${COMMON_WARN_FLAGS} ${C_WARN_FLAGS}")
+set(CMAKE_CXX_FLAGS_INIT "${COMMON_WARN_FLAGS} ${CXX_WARN_FLAGS}")
+
+set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc-13 CACHE INTERNAL "C compiler")
+set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++-13 CACHE INTERNAL "C++ compiler")
+set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-aarch64" "-L" "/usr/aarch64-linux-gnu/" CACHE INTERNAL "qemu")
