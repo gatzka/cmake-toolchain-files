dc91b4fc7b72a16de71995689eb040615bf7e106
Stephan Gatzka <stephan.gatzka@gmail.com>
2024-07-30 17:54:21 +0200

    Correct flags for code coverage generation.

--- compiler/gcc10.cmake
+++ compiler/gcc10.cmake
@@ -2,3 +2,4 @@ include("${CMAKE_CURRENT_LIST_DIR}/gcc8.cmake")
 
 set(C_WARN_FLAGS "${C_WARN_FLAGS} -fanalyzer")
 
+set(COVERAGE_FLAGS "${COVERAGE_FLAGS} -Wno-analyzer-fd-leak")
--- compiler/gcc5.cmake
+++ compiler/gcc5.cmake
@@ -2,4 +2,6 @@ include("${CMAKE_CURRENT_LIST_DIR}/clang_gcc.cmake")
 
 set(COMMON_WARN_FLAGS "${COMMON_WARN_FLAGS} -Warray-bounds=2")
 
+set(COVERAGE_FLAGS "${COVERAGE_FLAGS} -fno-inline-small-functions -fno-default-inline")
+
 set(CXX_WARN_FLAGS "${CXX_WARN_FLAGS} -Wnoexcept -Wstrict-null-sentinel")
