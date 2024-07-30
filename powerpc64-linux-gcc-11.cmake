9764cf2a004b63961bc2f470a10a88ed82461ad0
Stephan Gatzka <stephan.gatzka@gmail.com>
2024-07-30 12:27:37 +0200

    Bump arm ppc64 toolchain to latest versions.

---
+++ powerpc64-linux-gcc-11.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR ppc)
+
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc11.cmake")
+
+set(CMAKE_C_FLAGS_INIT "${COMMON_WARN_FLAGS} ${C_WARN_FLAGS}")
+set(CMAKE_CXX_FLAGS_INIT "${COMMON_WARN_FLAGS} ${CXX_WARN_FLAGS}")
+
+set(CMAKE_C_COMPILER powerpc64-linux-gnu-gcc-11 CACHE INTERNAL "C compiler")
+set(CMAKE_CXX_COMPILER powerpc64-linux-gnu-g++-11 CACHE INTERNAL "C++ compiler")
+set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-ppc64" "-L" "/usr/powerpc64-linux-gnu/" CACHE INTERNAL "qemu")
+
