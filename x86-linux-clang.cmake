f2b5ea4295d4db870c41b951ab21b20ceab8fab9
Stephan Gatzka <stephan.gatzka@gmail.com>
2024-07-30 12:20:32 +0200

    Bump generic gcc/clang toolchain files to latest stable releases.

--- x86-linux-clang.cmake
+++ x86-linux-clang.cmake
@@ -1,6 +1,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
-include("${CMAKE_CURRENT_LIST_DIR}/compiler/clang.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/clang18.cmake")
 
 set(CMAKE_C_FLAGS_INIT "${COMMON_WARN_FLAGS} ${C_WARN_FLAGS}")
 set(CMAKE_CXX_FLAGS_INIT "${COMMON_WARN_FLAGS} ${CXX_WARN_FLAGS}")
--- x86-linux-gcc.cmake
+++ x86-linux-gcc.cmake
@@ -1,6 +1,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
-include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc8.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc14.cmake")
 
 set(CMAKE_C_FLAGS_INIT "${COMMON_WARN_FLAGS} ${C_WARN_FLAGS}")
 set(CMAKE_CXX_FLAGS_INIT "${COMMON_WARN_FLAGS} ${CXX_WARN_FLAGS}")
